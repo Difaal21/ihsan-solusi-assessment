5f5da646d17653fd1a40cc9420caea91f28685a8
Difa Al Fansha <difaal21@gmail.com>
2025-02-20 09:16:37 +0700

    docs: update db docs

--- database/docs/ddl.sql
+++ database/docs/ddl.sql
@@ -21,11 +21,11 @@ CREATE TABLE public.users (
 -- DROP TABLE public.financial_accounts;
 
 CREATE TABLE public.financial_accounts (
-	id int4 DEFAULT nextval('user_transactions_id_seq'::regclass) NOT NULL,
+ 	id serial4 NOT NULL,
 	user_id int4 NOT NULL,
 	balance numeric(15, 2) DEFAULT 0.00 NOT NULL,
 	bank_account_number varchar(50) NOT NULL,
 	created_at timestamptz DEFAULT now() NOT NULL,
-	CONSTRAINT user_transactions_pkey PRIMARY KEY (id),
-	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE
+	CONSTRAINT financial_accounts_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE cascade
 );
